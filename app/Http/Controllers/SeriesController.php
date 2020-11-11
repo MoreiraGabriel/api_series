bb1534f1925193a9e1f05f26976471d0e6161deb
Gabriel Moreira <gabriel.moreira@novatendencia.localdomain>
2020-11-11 01:48:45 -0300

    Primeiro endpoint api/series.

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class SeriesController{
+    public function index(){
+        return [
+            "Peaky Blinders"
+        ];
+    }
+}
+
+?>
